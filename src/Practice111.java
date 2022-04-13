2584b9a924717202d85ebc6f39c84657749db6dd
zübeyir <zeynelzatari@gmail.com>
2022-04-13 22:19:08 +0200

    initial commit

---
+++ src/Practice111.java
@@ -0,0 +1,27 @@
+public class Practice111 { // burayi yanlis yazdik yada degistirmek gerekiyor sa Amend
+    //commit yapiyoruz, ve histrory actigimizda da 1 tane dosya gönermis olacaz
+
+
+}
+/*
+## Git Day 3:
+
+1. create java proj called 'git_practice4'
+2. add '.gitignore' file with bellow content
+ ''''.idea
+            out
+            *.iml'''
+3. enable version conrol to create new repo for this proj
+4. make your initial commit
+5. create new class called ' pracatice1'
+6. make few changes and few commit
+7. und do your last commit
+
+
+
+## Github
+1. create new repository in Github.com
+2. learn to make it public or private
+3.make sure do not add anything into your newly created github repo to avoid furthur issues
+4. now back to your local Intellij, create connection between your local repo and remote repo
+ */
